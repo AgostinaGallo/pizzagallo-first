89e19d8d0bda1f875398f920f3e07f9d92948dc4
Agostina Gallo <agostinamgallo@gmail.com>
2021-07-26 17:18:48 -0300

    Added categories Seeder

---
+++ database/seeds/CategoriesTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+class CategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('categoria')->insert([
+            'nombre' => 'Insumos',
+            'descripcion' => 'Productos obtenidos para elaboracion',
+            'condicion' => 1,
+        ]);
+
+        DB::table('categoria')->insert([
+            'nombre' => 'Elaborados para venta',
+            'descripcion' => 'Productos para vender elaborados con insumos',
+            'condicion' => 1,
+        ]);
+
+        DB::table('categoria')->insert([
+            'nombre' => 'Reventa',
+            'descripcion' => 'Productos comprados para reventa',
+            'condicion' => 1,
+        ]);
+    }
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,16 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        if (app()->environment('local')) {
+            $this->askForAddSampleData();
+        }
+    }
+
+    protected function askForAddSampleData()
+    {
+        if ($this->command->confirm('¿Desea agregar todos los datos de prueba?')) {
+            $this->call(CategoriesTableSeeder::class);
+            $this->command->info('Categorias añadidas');
+        }
     }
 }
