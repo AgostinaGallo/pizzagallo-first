76bf3dd1108d292719ee5c9c701d6a891d494171
Agostina Gallo <agostinamgallo@gmail.com>
2021-07-26 15:56:38 -0300

    new connection

--- config/database.php
+++ config/database.php
@@ -55,8 +55,8 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'dbpizzagallo'),
+            'port' => env('DB_PORT', '3307'),
+            'database' => env('DB_DATABASE', 'dbpizzalaravel'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
