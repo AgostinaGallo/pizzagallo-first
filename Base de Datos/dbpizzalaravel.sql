cd6bf58e2542dd153c6c3bf6c052c2e6bc294b1d
Agostina Gallo <agostinamgallo@gmail.com>
2021-07-26 16:59:42 -0300

    new db script: added total_ingreso + preload data

--- Base de Datos/dbpizzalaravel.sql
+++ Base de Datos/dbpizzalaravel.sql
@@ -1,159 +1,295 @@
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1:3307
+-- Tiempo de generación: 26-07-2021 a las 20:54:52
+-- Versión del servidor: 10.4.18-MariaDB
+-- Versión de PHP: 7.4.16
 
-CREATE SCHEMA IF NOT EXISTS `dbpizzalaravel` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
-USE `dbpizzalaravel` ;
-
--- -----------------------------------------------------
--- Table `dbpizzalaravel`.`categoria`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `dbpizzalaravel`.`categoria` (
-  `idcategoria` INT NOT NULL AUTO_INCREMENT ,
-  `nombre` VARCHAR(50) NOT NULL ,
-  `descripcion` VARCHAR(256) NULL ,
-  `condicion` TINYINT(1) NOT NULL ,
-  PRIMARY KEY (`idcategoria`) )
-ENGINE = InnoDB;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
--- -----------------------------------------------------
--- Table `dbpizzalaravel`.`articulo`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `dbpizzalaravel`.`articulo` (
-  `idarticulo` INT NOT NULL AUTO_INCREMENT ,
-  `idcategoria` INT NOT NULL ,
-  `codigo` VARCHAR(50) NULL ,
-  `nombre` VARCHAR(100) NOT NULL ,
-  `stock` INT NOT NULL ,
-  `descripcion` VARCHAR(512) NULL ,
-  `imagen` VARCHAR(50) NULL ,
-  `estado` VARCHAR(20) NOT NULL ,
-  PRIMARY KEY (`idarticulo`) ,
-  INDEX `fk_articulo_categoria_idx` (`idcategoria` ASC) ,
-  CONSTRAINT `fk_articulo_categoria`
-    FOREIGN KEY (`idcategoria` )
-    REFERENCES `dbpizzalaravel`.`categoria` (`idcategoria` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Base de datos: `dbpizzalaravel`
+--
 
--- -----------------------------------------------------
--- Table `dbpizzalaravel`.`persona`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `dbpizzalaravel`.`persona` (
-  `idpersona` INT NOT NULL AUTO_INCREMENT ,
-  `tipo_persona` VARCHAR(20) NOT NULL ,
-  `nombre` VARCHAR(100) NOT NULL ,
-  `tipo_documento` VARCHAR(20) NULL ,
-  `num_documento` VARCHAR(15) NULL ,
-  `direccion` VARCHAR(70) NULL ,
-  `telefono` VARCHAR(15) NULL ,
-  `email` VARCHAR(50) NULL ,
-  PRIMARY KEY (`idpersona`) )
-ENGINE = InnoDB;
+-- --------------------------------------------------------
 
+--
+-- Estructura de tabla para la tabla `articulo`
+--
 
--- -----------------------------------------------------
--- Table `dbpizzalaravel`.`ingreso`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `dbpizzalaravel`.`ingreso` (
-  `idingreso` INT NOT NULL AUTO_INCREMENT ,
-  `idproveedor` INT NOT NULL ,
-  `tipo_comprobante` VARCHAR(20) NOT NULL ,
-  `serie_comprobante` VARCHAR(7) NULL ,
-  `num_comprobante` VARCHAR(10) NOT NULL ,
-  `fecha_hora` DATETIME NOT NULL ,
-  `impuesto` DECIMAL(4,2) NOT NULL ,
-  `estado` VARCHAR(20) NOT NULL ,
-  PRIMARY KEY (`idingreso`) ,
-  INDEX `fk_ingreso_persona_idx` (`idproveedor` ASC) ,
-  CONSTRAINT `fk_ingreso_persona`
-    FOREIGN KEY (`idproveedor` )
-    REFERENCES `dbpizzalaravel`.`persona` (`idpersona` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+CREATE TABLE `articulo` (
+  `idarticulo` int(11) NOT NULL,
+  `idcategoria` int(11) NOT NULL,
+  `codigo` varchar(50) DEFAULT NULL,
+  `unidad_medida` varchar(50) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `stock` int(11) NOT NULL,
+  `descripcion` varchar(512) DEFAULT NULL,
+  `imagen` varchar(50) DEFAULT NULL,
+  `estado` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
 
--- -----------------------------------------------------
--- Table `dbpizzalaravel`.`detalle_ingreso`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `dbpizzalaravel`.`detalle_ingreso` (
-  `iddetalle_ingreso` INT NOT NULL AUTO_INCREMENT ,
-  `idingreso` INT NOT NULL ,
-  `idarticulo` INT NOT NULL ,
-  `cantidad` INT NOT NULL ,
-  `precio_compra` DECIMAL(11,2) NOT NULL ,
-  `precio_venta` DECIMAL(11,2) NOT NULL ,
-  PRIMARY KEY (`iddetalle_ingreso`) ,
-  INDEX `fk_detalle_ingreso_idx` (`idingreso` ASC) ,
-  INDEX `fk_detalle_ingreso_articulo_idx` (`idarticulo` ASC) ,
-  CONSTRAINT `fk_detalle_ingreso`
-    FOREIGN KEY (`idingreso` )
-    REFERENCES `dbpizzalaravel`.`ingreso` (`idingreso` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_detalle_ingreso_articulo`
-    FOREIGN KEY (`idarticulo` )
-    REFERENCES `dbpizzalaravel`.`articulo` (`idarticulo` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+--
+-- Estructura de tabla para la tabla `categoria`
+--
 
+CREATE TABLE `categoria` (
+  `idcategoria` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `descripcion` varchar(256) DEFAULT NULL,
+  `condicion` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- -----------------------------------------------------
--- Table `dbpizzalaravel`.`venta`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `dbpizzalaravel`.`venta` (
-  `idventa` INT NOT NULL AUTO_INCREMENT ,
-  `idcliente` INT NOT NULL ,
-  `tipo_comprobante` VARCHAR(20) NOT NULL ,
-  `serie_comprobante` VARCHAR(7) NOT NULL ,
-  `num_comprobante` VARCHAR(10) NOT NULL ,
-  `fecha_hora` DATETIME NOT NULL ,
-  `impuesto` DECIMAL(4,2) NOT NULL ,
-  `total_venta` DECIMAL(11,2) NOT NULL ,
-  `estado` VARCHAR(20) NOT NULL ,
-  PRIMARY KEY (`idventa`) ,
-  INDEX `fk_venta_cliente_idx` (`idcliente` ASC) ,
-  CONSTRAINT `fk_venta_cliente`
-    FOREIGN KEY (`idcliente` )
-    REFERENCES `dbpizzalaravel`.`persona` (`idpersona` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+--
+-- Volcado de datos para la tabla `categoria`
+--
 
+INSERT INTO `categoria` (`idcategoria`, `nombre`, `descripcion`, `condicion`) VALUES
+(1, 'Insumos', 'Productos obtenidos para elaboracion', 1),
+(2, 'Elaborados para venta', 'Productos para vender elaborados con insumos', 1),
+(3, 'Reventa', 'Productos comprados para reventa', 1);
 
--- -----------------------------------------------------
--- Table `dbpizzalaravel`.`detalle_venta`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `dbpizzalaravel`.`detalle_venta` (
-  `iddetalle_venta` INT NOT NULL AUTO_INCREMENT ,
-  `idventa` INT NOT NULL ,
-  `idarticulo` INT NOT NULL ,
-  `cantidad` INT NOT NULL ,
-  `precio_venta` DECIMAL(11,2) NOT NULL ,
-  `descuento` DECIMAL(11,2) NOT NULL ,
-  PRIMARY KEY (`iddetalle_venta`) ,
-  INDEX `fk_detalle_venta_articulo_idx` (`idarticulo` ASC) ,
-  INDEX `fk_detalle_venta_idx` (`idventa` ASC) ,
-  CONSTRAINT `fk_detalle_venta_articulo`
-    FOREIGN KEY (`idarticulo` )
-    REFERENCES `dbpizzalaravel`.`articulo` (`idarticulo` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_detalle_venta`
-    FOREIGN KEY (`idventa` )
-    REFERENCES `dbpizzalaravel`.`venta` (`idventa` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+-- --------------------------------------------------------
 
-USE `dbpizzalaravel` ;
+--
+-- Estructura de tabla para la tabla `detalle_ingreso`
+--
 
+CREATE TABLE `detalle_ingreso` (
+  `iddetalle_ingreso` int(11) NOT NULL,
+  `idingreso` int(11) NOT NULL,
+  `idarticulo` int(11) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `precio_compra` decimal(11,2) NOT NULL,
+  `precio_venta` decimal(11,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detalle_venta`
+--
+
+CREATE TABLE `detalle_venta` (
+  `iddetalle_venta` int(11) NOT NULL,
+  `idventa` int(11) NOT NULL,
+  `idarticulo` int(11) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `precio_venta` decimal(11,2) NOT NULL,
+  `descuento` decimal(11,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ingreso`
+--
+
+CREATE TABLE `ingreso` (
+  `idingreso` int(11) NOT NULL,
+  `idproveedor` int(11) NOT NULL,
+  `tipo_comprobante` varchar(20) NOT NULL,
+  `serie_comprobante` varchar(7) DEFAULT NULL,
+  `num_comprobante` varchar(10) NOT NULL,
+  `fecha_hora` datetime NOT NULL,
+  `impuesto` decimal(4,2) NOT NULL,
+  `total_ingreso` decimal(11,2) NOT NULL,
+  `estado` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `persona`
+--
+
+CREATE TABLE `persona` (
+  `idpersona` int(11) NOT NULL,
+  `tipo_persona` varchar(20) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `tipo_documento` varchar(20) DEFAULT NULL,
+  `num_documento` varchar(15) DEFAULT NULL,
+  `direccion` varchar(70) DEFAULT NULL,
+  `telefono` varchar(15) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `fecha_nacimiento` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `persona`
+--
+
+INSERT INTO `persona` (`idpersona`, `tipo_persona`, `nombre`, `tipo_documento`, `num_documento`, `direccion`, `telefono`, `email`, `fecha_nacimiento`) VALUES
+(1, 'Empleado', 'Juan Perez', 'DNI', '30555444', 'Barrio Los paraisos ', '3764554430', 'juanperez@gmail.com', '1980-07-06'),
+(2, 'Proveedor', 'Maxiconsumo', 'CUIT', '30663005843', 'Av. Quaranta', '44002255', 'maxiconsumo@gmail.com', NULL),
+(3, 'Cliente', 'Marianela Estevanez', 'DNI', '35401204', '2545 Cabred y Av. Uruguay ', '3764158789', 'marianelaestevanez@hotmail.com', '1995-12-20');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `venta`
+--
+
+CREATE TABLE `venta` (
+  `idventa` int(11) NOT NULL,
+  `idcliente` int(11) NOT NULL,
+  `tipo_comprobante` varchar(20) NOT NULL,
+  `serie_comprobante` varchar(7) NOT NULL,
+  `num_comprobante` varchar(10) NOT NULL,
+  `fecha_hora` datetime NOT NULL,
+  `impuesto` decimal(4,2) NOT NULL,
+  `total_venta` decimal(11,2) NOT NULL,
+  `estado` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `articulo`
+--
+ALTER TABLE `articulo`
+  ADD PRIMARY KEY (`idarticulo`),
+  ADD KEY `fk_articulo_categoria_idx` (`idcategoria`);
+
+--
+-- Indices de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  ADD PRIMARY KEY (`idcategoria`);
+
+--
+-- Indices de la tabla `detalle_ingreso`
+--
+ALTER TABLE `detalle_ingreso`
+  ADD PRIMARY KEY (`iddetalle_ingreso`),
+  ADD KEY `fk_detalle_ingreso_idx` (`idingreso`),
+  ADD KEY `fk_detalle_ingreso_articulo_idx` (`idarticulo`);
+
+--
+-- Indices de la tabla `detalle_venta`
+--
+ALTER TABLE `detalle_venta`
+  ADD PRIMARY KEY (`iddetalle_venta`),
+  ADD KEY `fk_detalle_venta_articulo_idx` (`idarticulo`),
+  ADD KEY `fk_detalle_venta_idx` (`idventa`);
+
+--
+-- Indices de la tabla `ingreso`
+--
+ALTER TABLE `ingreso`
+  ADD PRIMARY KEY (`idingreso`),
+  ADD KEY `fk_ingreso_persona_idx` (`idproveedor`);
+
+--
+-- Indices de la tabla `persona`
+--
+ALTER TABLE `persona`
+  ADD PRIMARY KEY (`idpersona`);
+
+--
+-- Indices de la tabla `venta`
+--
+ALTER TABLE `venta`
+  ADD PRIMARY KEY (`idventa`),
+  ADD KEY `fk_venta_cliente_idx` (`idcliente`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `articulo`
+--
+ALTER TABLE `articulo`
+  MODIFY `idarticulo` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  MODIFY `idcategoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `detalle_ingreso`
+--
+ALTER TABLE `detalle_ingreso`
+  MODIFY `iddetalle_ingreso` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `detalle_venta`
+--
+ALTER TABLE `detalle_venta`
+  MODIFY `iddetalle_venta` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `ingreso`
+--
+ALTER TABLE `ingreso`
+  MODIFY `idingreso` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `persona`
+--
+ALTER TABLE `persona`
+  MODIFY `idpersona` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `venta`
+--
+ALTER TABLE `venta`
+  MODIFY `idventa` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `articulo`
+--
+ALTER TABLE `articulo`
+  ADD CONSTRAINT `fk_articulo_categoria` FOREIGN KEY (`idcategoria`) REFERENCES `categoria` (`idcategoria`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `detalle_ingreso`
+--
+ALTER TABLE `detalle_ingreso`
+  ADD CONSTRAINT `fk_detalle_ingreso` FOREIGN KEY (`idingreso`) REFERENCES `ingreso` (`idingreso`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_detalle_ingreso_articulo` FOREIGN KEY (`idarticulo`) REFERENCES `articulo` (`idarticulo`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `detalle_venta`
+--
+ALTER TABLE `detalle_venta`
+  ADD CONSTRAINT `fk_detalle_venta` FOREIGN KEY (`idventa`) REFERENCES `venta` (`idventa`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_detalle_venta_articulo` FOREIGN KEY (`idarticulo`) REFERENCES `articulo` (`idarticulo`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `ingreso`
+--
+ALTER TABLE `ingreso`
+  ADD CONSTRAINT `fk_ingreso_persona` FOREIGN KEY (`idproveedor`) REFERENCES `persona` (`idpersona`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `venta`
+--
+ALTER TABLE `venta`
+  ADD CONSTRAINT `fk_venta_cliente` FOREIGN KEY (`idcliente`) REFERENCES `persona` (`idpersona`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
